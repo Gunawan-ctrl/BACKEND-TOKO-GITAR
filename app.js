ebcd1074e63e2fe0555c5f622ddc1ee3ffc2aa77
Gunawan-ctrl <gunawan.dev@gmail.com>
2024-01-23 20:44:09 +0700

    menambahkan modul cors

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const app = express();
+const cors = require("cors"); // Impor modul cors
 const dbConfig = require("./config/db_config");
 const mongoose = require("mongoose");
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect( dbConfig.mongoUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log("Terhubung ke Mongodb");
-}).catch((err) => {
-  console.log(err);
-  console.log("Gagal terhubung ke Mongodb");
-});
+// Menambahkan middleware cors
+app.use(cors());
+
+mongoose
+  .connect(dbConfig.mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Terhubung ke Mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log("Gagal terhubung ke Mongodb");
+  });
 
 app.use("/users", require("./routes/userRoute"));
 app.use("/kategori", require("./routes/kategoriRoute"));
--- routes/barangRoute.js
+++ routes/barangRoute.js
@@ -4,69 +4,73 @@ const barangController = require("../controllers/barangController");
 const utilApps = require("../utils/utils_apps");
 const multer = require("multer");
 const uploadFile = multer({
-  storage: utilApps.uploadFile
+  storage: utilApps.uploadFile,
 }).single("gambar");
 
-
-router.post("/create-barang", uploadFile, (req, res) => {
-  if(req.file === undefined) {
+router.post("/input", uploadFile, (req, res) => {
+  if (req.file === undefined) {
     res.json({
       status: false,
-      msg: "Gambar tidak boleh kosong"
-    })
+      msg: "Gambar tidak boleh kosong",
+    });
   } else {
-    req.body.gambar = req.file.filename
+    req.body.gambar = req.file.filename;
   }
-  barangController.input(req.body)
+  barangController
+    .input(req.body)
     .then((result) => {
       res.json(result);
     })
     .catch((error) => {
       res.json(error);
-    })
-})
+    });
+});
 
 router.get("/get-all-barang", (req, res) => {
-  barangController.getAllBarang()
+  barangController
+    .getAllBarang()
     .then((result) => {
       res.json(result);
     })
     .catch((err) => {
       res.json(err);
-    })
-})
+    });
+});
 
 router.get("/get-barang-by-id/:idBarang", (req, res) => {
-  barangController.getBarangById(req.params.idBarang)
+  barangController
+    .getBarangById(req.params.idBarang)
     .then((result) => {
       res.json(result);
     })
     .catch((err) => {
       res.json(err);
-    })
-})
+    });
+});
 
-router.put("/update-barang/:idBarang", uploadFile, (req, res) => {
+router.put("/update/:idBarang", uploadFile, (req, res) => {
   if (req.file !== undefined) {
-    req.body.gambar = req.file.filename
+    req.body.gambar = req.file.filename;
   }
-  barangController.update(req.params.idBarang, req.body)
+  barangController
+    .update(req.params.idBarang, req.body)
     .then((result) => {
       res.json(result);
     })
     .catch((err) => {
       res.json(err);
-    })
-})
+    });
+});
 
 router.delete("/delete-barang/:idBarang", (req, res) => {
-  barangController.delete(req.params.idBarang)
+  barangController
+    .delete(req.params.idBarang)
     .then((result) => {
       res.json(result);
     })
     .catch((err) => {
       res.json(err);
-    })
-})
+    });
+});
 
 module.exports = router;
